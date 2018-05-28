b35ad1c9082b11f30bb1691c6dbc0fa6c89c570e
KelliePetersen <KellieAnnPetersen@gmail.com>
2018-05-28 14:52:56 -0400

    add computer player with AI

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ window.onload = function() {
   canvasContext = canvas.getContext('2d');
 
   setInterval(function() {
+    computerMovement();
     moveBall();
     drawGame();
   }, 1000/30);
 
   canvas.addEventListener('mousemove', function(event) {
     let mousePosition = calculateMousePosition(event);
-    paddleY = mousePosition.y;
+    paddleY = mousePosition.y - paddleHeight/2;
   });
 };
 
@@ -25,14 +26,32 @@ let ballSpeedY = 5;
 
 // Player
 const paddleHeight = 100;
+const paddleWidth = 25;
 let paddleY = 250;
+
+let compPaddleY = 250;
+
+let playerScore = 0;
+let computerScore = 0;
 
 function moveBall() {
   if (ballX > canvas.width) {
     ballSpeedX = -ballSpeedX;
+    if (ballY > compPaddleY && ballY < compPaddleY + paddleHeight) {
+      computerScore += 1;
+    } else {
+      resetBall();
+      computerScore = 0;
+    }
   } 
   if (ballX < 0) {
     ballSpeedX = -ballSpeedX;
+    if (ballY > paddleY && ballY < paddleY + paddleHeight) {
+      playerScore += 1;
+    } else {
+      resetBall();
+      playerScore = 0;
+    }
   }
   if (ballY > canvas.height) {
     ballSpeedY = -ballSpeedY;
@@ -46,10 +65,11 @@ function moveBall() {
 
 function drawGame() {
   drawRect(0, 0, canvas.width, canvas.height, 'black');
-  // Turn into ball object/balls constructor, with random traits
   drawCircle(ballX, ballY, 10, 'white');
-  // Turn into player object
-  drawRect(0, paddleY, 25, 50, 'white');
+  drawRect(0, paddleY, paddleWidth, paddleHeight, 'white');
+  drawRect(canvas.width-paddleWidth, compPaddleY, paddleWidth, paddleHeight, 'white');
+  canvasContext.fillText(playerScore, 100, 100);
+  canvasContext.fillText(computerScore, 700, 100);
 }
 
 function drawCircle(x, y, radius, color) {
@@ -73,4 +93,18 @@ function calculateMousePosition(event) {
     x: mouseX,
     y: mouseY
   };
+}
+
+function resetBall() {
+  ballX = canvas.width/2;
+  ballY = canvas.height/2;
+}
+
+function computerMovement() {
+  let compPaddleYCenter = compPaddleY + paddleHeight/2;
+  if (compPaddleYCenter < ballY - 35) {
+    compPaddleY += 5;
+  } else if (compPaddleYCenter > ballY + 35) {
+    compPaddleY -= 5;
+  }
 }
